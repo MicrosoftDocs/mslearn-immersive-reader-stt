bab3f4dda94caf6f11673c7019d053ff7fc99f6f
Seth Reid <seth.reid@xero.com>
2021-06-20 09:42:03 +1200

    remove

--- src/ImmersiveReaderDemo/ClientApp/src/api/settings.js
+++ src/ImmersiveReaderDemo/ClientApp/src/api/settings.js
@@ -1,13 +1,5 @@
-import { HTTP_STATUS_CODES } from "../constants";
-
 export async function fetchSetting() {
     const response = await fetch("/settings");
-    if (
-        response.status === HTTP_STATUS_CODES.Unauthorized ||
-        response.status === HTTP_STATUS_CODES.NotFound
-    ) {
-        return undefined;
-    }
     if (!response.ok) {
         throw new Error("Error loading the access token.");
     }
